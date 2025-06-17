910de4ce9f452c20a4ce6e3093bb15e277d1d64f
Toni Kleinfeld <Evil_poring@hotmail.de>
2025-06-17 10:21:57 +0200

    style : new header design

--- components/logo.vue
+++ components/logo.vue
@@ -1,34 +1,42 @@
 <template>
     <div class="logo-wrapper">
         <img src="../assets/images/logo.svg">
-        <h1>Münster Interessiert's</h1>
-        <h3>Dein Info-Podcast aus dem Rat</h3>
-
+        <div class="logo-text-wrapper">
+            <h1>Münster Interessiert's</h1>
+            <h3>Dein Info-Podcast aus dem Rat</h3>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .logo-wrapper {
-    @include mix.center();
-    flex-direction: column;
-    padding: 24px 0;
+    @include mix.center($g:10px);
+    flex-direction: row;
+    padding: 15px ;
+    margin-top: 10px;
+
+    .logo-text-wrapper{
+        @include mix.center($jc:flex-start,  $ai:flex-start);
+        flex-direction: column;
+    }
 
     img {
-        margin: 5vh 0 16px 40px;
+        height: 50px;
+        width: 56px;
     }
 
     h1 {
-        font-size: 60px;
+        font-size: 22px;
         font-weight: 700;
         letter-spacing: 5px;
     }
 
     h3 {
         margin-top: -8px;
-        font-size: 30px;
+        font-size: 23px;
         font-weight: 200;
-        word-spacing: 12px;
-        letter-spacing: 10px;
+        word-spacing: 5px;
+        letter-spacing: 8px;
     }
 }
 </style>
