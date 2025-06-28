ad6774dc2940b894136ec27e0a451bea2ce7fec9
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-28 16:26:30 +0200

    Try fix github deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+  },
+  nitro: {
+    prerender: {
+      routes: ["/"],
+    },
+  },
   compatibilityDate: "2024-11-01",
   ssr: false,
   devtools: { enabled: true },
